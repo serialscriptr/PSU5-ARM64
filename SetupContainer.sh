aee05a3549b530046d0ec22b9121c602f73bb4fe
Ryan McAvoy <62270481+serialscriptr@users.noreply.github.com>
2025-06-02 20:15:22 -0400

    Update SetupContainer.sh

--- SetupContainer.sh
+++ SetupContainer.sh
@@ -2,15 +2,17 @@
 
 # ensure prereqs are met at this point, install with RUN in Dockerfile, these are for ubuntu 24.x
 # wget curl unzip libc6 libgcc1 libgcc-s1 libgssapi-krb5-2 libicu74 liblttng-ust1 libssl3 libstdc++6 libunwind8 zlib1g openssl
-apt update
-apt install acl -y
+#apt update
+#apt install acl -y
 # pwsh latest arm64
 pwshurl=$(curl https://api.github.com/repos/PowerShell/PowerShell/releases/latest | grep -oP 'https://github.com/PowerShell/PowerShell/releases/download/.*linux-arm64.tar.gz' | head -1)
-wget $pwshurl
-mkdir powershell
-pwshtar=$(ls | grep -oP 'powershell.*-linux-arm64.tar.gz' | head -1)
+#wget $pwshurl
+curl -L $pwshurl --output pwsh.tar.gz
+#mkdir powershell
+#pwshtar=$(ls | grep -oP 'powershell.*-linux-arm64.tar.gz' | head -1)
 #tar -xvf $pwshtar -C ./powershell --no-same-owner
-tar zxf $pwshtar -C /opt/microsoft/powershell/7 --no-same-owner
+#tar zxf $pwshtar -C /opt/microsoft/powershell/7 --no-same-owner
+tar zxf pwsh.tar.gz -C /opt/microsoft/powershell/7
 #ln -s ./powershell/pwsh /usr/bin/pwsh
 chmod +x /opt/microsoft/powershell/7/pwsh
 #chmod +x ./powershell/pwsh
@@ -29,10 +31,11 @@ echo "Creating $PSU_PATH and granting access to user $USER"
 if [ ! -f $PSU_PATH ]; then
   mkdir $PSU_PATH
 fi
-chown $PSU_USER -R $PSU_PATH -v
-chown $PSU_USER -R "/home/psuniversal/" -v
-setfacl -m "u:${USER}:rwx" "/home/psuniversal/.PowerShellUniversal/"
-setfacl -m "u:${USER}:rwx" $PSU_PATH
+chown $PSU_USER -R $PSU_PATH
+chown $PSU_USER -R "/home/psuniversal/"
+chown $PSU_USER -R "/opt/microsoft/powershell/"
+#setfacl -m "u:${USER}:rwx" "/home/psuniversal/.PowerShellUniversal/"
+#setfacl -m "u:${USER}:rwx" $PSU_PATH
 echo "Make $PSU_EXEC executable"
 chmod +x $PSU_EXEC
 
